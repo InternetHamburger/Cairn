cb0b0308d0c8dd61efd1eb397908758bf593426e
wolden.sindre@gmail.com <wolden.sindre@gmail.com>
2026-05-08 16:55:05 +0200

    Implement basic attacks

--- src/preComputedData.c
+++ src/preComputedData.c
@@ -2,6 +2,7 @@
 
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
 
 unsigned long long knight_moves[64];
 unsigned long long king_moves[64];
@@ -10,6 +11,66 @@ const uint64_t a_file = 0x0101010101010101;
 const uint64_t first_rank = 0xff00000000000000;
 
 uint64_t rays[64][8];
+uint64_t rank_attacks[256][8];
+smsk masks;
+
+uint64_t diagonalMask(int sq) {
+    const uint64_t maindia = 0x8040201008040201;
+    int diag  = (sq&7) - (sq>>3);
+    return diag >= 0 ? maindia >> diag*8 : maindia << -diag*8;
+}
+
+uint64_t antiDiagMask(int sq) {
+    const uint64_t maindia = 0x0102040810204080;
+    int diag  = 7 - (sq&7) - (sq>>3);
+    return diag >= 0 ? maindia >> diag*8 : maindia << -diag*8;
+}
+
+uint64_t diagonalAttacks(uint64_t occ, int sq) {
+    uint64_t forward, reverse;
+    forward = occ & masks[sq].diagonalMaskEx;
+    reverse  = _byteswap_uint64(forward);
+    forward -= masks[sq].bitMask;
+    reverse -= _byteswap_uint64(masks[sq].bitMask);
+    forward ^= _byteswap_uint64(reverse);
+    forward &= masks[sq].diagonalMaskEx;
+    return forward;
+}
+
+uint64_t antiDiagAttacks(uint64_t occ, int sq) {
+    uint64_t forward, reverse;
+    forward  = occ & masks[sq].antidiagMaskEx;
+    reverse  = _byteswap_uint64(forward);
+    forward -= masks[sq].bitMask;
+    reverse -= _byteswap_uint64(masks[sq].bitMask);
+    forward ^= _byteswap_uint64(reverse);
+    forward &= masks[sq].antidiagMaskEx;
+    return forward;
+}
+
+uint64_t fileAttacks(uint64_t occ, int sq) {
+    uint64_t forward, reverse;
+    forward  = occ & masks[sq].fileMaskEx;
+    reverse  = _byteswap_uint64(forward);
+    forward -= masks[sq].bitMask;
+    reverse -= _byteswap_uint64(masks[sq].bitMask);
+    forward ^= _byteswap_uint64(reverse);
+    forward &= masks[sq].fileMaskEx;
+    return forward;
+}
+
+uint64_t rankAttacks(uint64_t occ, int sq)
+{
+    return rank_attacks[occ >> (8 * (sq >> 3)) & 0b11111111][sq & 7] << (8 * (sq >> 3));
+}
+
+uint64_t allAttacks(uint64_t occ, int sq)
+{
+    return diagonalAttacks(occ, sq) +
+           antiDiagAttacks(occ, sq) +
+           fileAttacks(occ, sq) +
+           rankAttacks(occ, sq);
+}
 
 __attribute__((constructor))  // runs before main()
 static void init_table(void) {
@@ -24,6 +85,56 @@ static void init_table(void) {
     // right-up, left-up, right-down, left-down, right, left, up, down
     const int rank_directions[] = {-1, -1, 1, 1, 0, 0, -1, 1};
     const int file_directions[] = {-1, 1, -1, 1, 1, -1, 0, 0};
+
+    uint64_t files[8] = {0};
+
+    for (int i = 0; i < 8; i++)
+    {
+        files[i] = a_file << i;
+    }
+
+    for (int i = 0; i < 64; i++)
+    {
+        masks[i].bitMask = 1ULL << i;
+        masks[i].fileMaskEx = files[i % 8];
+        masks[i].antidiagMaskEx = antiDiagMask(i) - (1ULL << i);
+        masks[i].diagonalMaskEx = diagonalMask(i) - (1ULL << i);
+    }
+
+    for (uint32_t i = 0; i < 256; i++)
+    {
+        for (int sq = 0; sq < 8; sq++)
+        {
+            int file = sq;
+            uint64_t attacks = 0;
+            while (1)
+            {
+                file++;
+                if (file >= 8)
+                    break;
+
+                attacks |= 1ULL << file;
+
+                if (1ULL << file & i)
+                    break;
+            }
+
+            file = sq;
+            while (1)
+            {
+                file--;
+                if (file < 0)
+                    break;
+
+                attacks |= 1ULL << file;
+
+                if (1ULL << file & i)
+                    break;
+            }
+
+            rank_attacks[i][sq] = attacks;
+        }
+    }
 
     for (int i = 0; i < 64; i++) {
         const int file = i % 8;
--- src/preComputedData.h
+++ src/preComputedData.h
@@ -4,10 +4,25 @@
 #include <stdint.h>
 
 #define TABLE_SIZE 64
+
+typedef struct
+{
+    uint64_t bitMask;
+    uint64_t diagonalMaskEx;
+    uint64_t antidiagMaskEx;
+    uint64_t fileMaskEx;
+} smsk[64];
+
+uint64_t diagonalAttacks(uint64_t occ, int sq);
+uint64_t antiDiagAttacks(uint64_t occ, int sq);
+uint64_t fileAttacks(uint64_t occ, int sq);
+uint64_t allAttacks(uint64_t occ, int sq);
+
 extern uint64_t knight_moves[TABLE_SIZE];
 extern uint64_t king_moves[TABLE_SIZE];
 extern const uint64_t a_file;
 extern const uint64_t first_rank;
 extern uint64_t rays[TABLE_SIZE][8];
+extern smsk masks;
 
 #endif //CAIRN_PRECOMPUTEDDATA_H
