492e1d0f45e55ea87c86a16892730010312c709e
wolden.sindre@gmail.com <wolden.sindre@gmail.com>
2026-05-12 13:34:38 +0200

    Use precomputed magics

--- src/preComputedData.c
+++ src/preComputedData.c
@@ -17,6 +17,13 @@ uint64_t rays[64][8];
 uint64_t rank_attacks[256][8];
 smsk masks;
 
+uint64_t rook_magic_numbers[64] = {
+    6665332121435906180ULL, 2323858233967513600ULL, 180152918636233344ULL, 5116093643460970752ULL, 1297046055996751904ULL, 72058693684035586ULL, 15168264832261554432ULL, 1513209758272729344ULL, 74872489842327808ULL, 4611756388513751040ULL, 5478770235550994432ULL, 2378182146981953568ULL, 36310581234171952ULL, 72902036148454144ULL, 10133167916974376ULL, 38421383715767040ULL, 4629709762790433408ULL, 18014950414876736ULL, 282575564177424ULL, 63191682295826432ULL, 4900198970275401740ULL, 9223654611947104258ULL, 288234774467913826ULL, 144152571496960324ULL, 10953950581743520358ULL, 360358341083398272ULL, 144150376744026176ULL, 22518105513136384ULL, 2252351721704449ULL, 4400194125952ULL, 580593679157386ULL, 2449972018494571073ULL, 36029071901073412ULL, 5764757881257730048ULL, 457398993031174ULL, 1706444202182656ULL, 1175580257420314624ULL, 4616752705514504208ULL, 11613248038110472ULL, 1225119837215260928ULL, 27079323198455808ULL, 156078295643979840ULL, 281750391685144ULL, 72339211017060360ULL, 4611704710259343381ULL, 2451106087462535296ULL, 4612249127697383492ULL, 40620911648440324ULL, 36100267422261504ULL, 2882451370955247744ULL, 3170569326340125440ULL, 288652966577045632ULL, 9368626320185000064ULL, 338332956634644992ULL, 72059797626168320ULL, 72339077614027008ULL, 18226054514610465ULL, 1297371357608345729ULL, 11854046244460560401ULL, 18023280770351137ULL, 3460171957490812929ULL, 576742519339493889ULL, 90073126855282692ULL, 576887380536231174ULL, };
+
+uint64_t bishop_magic_numbers[64] = {
+    2310488454833766432ULL, 585824197654094336ULL, 145267478493134994ULL, 9802089609931280384ULL, 9549322327662002432ULL, 2307268020040892449ULL, 4611765218770460672ULL, 9241599760215904328ULL, 28862318289617168ULL, 4035418789968953472ULL, 9260123384912562176ULL, 576473998007738416ULL, 4611690434190704640ULL, 36874339179888640ULL, 6919781394659415064ULL, 576478628091594752ULL, 4513512697501696ULL, 14062507583537709568ULL, 6346838546722324864ULL, 2306968914019517441ULL, 9289258884399616ULL, 1153203538197872900ULL, 6917740279920347138ULL, 618734921430733324ULL, 72796484106977792ULL, 1166437251425567744ULL, 4756083789849969154ULL, 1158552382892739712ULL, 4785143340351488ULL, 2328502299024428048ULL, 6939769953483752512ULL, 1145184360352004ULL, 5057545817511969824ULL, 38291600623801348ULL, 74878546006378496ULL, 117185966468760080ULL, 299084343443488ULL, 20274999248097287ULL, 9224502626874392710ULL, 5067258251575360ULL, 1153207388905422848ULL, 1171015360078417920ULL, 565149580745226ULL, 18014673660052992ULL, 306253609446671361ULL, 27062425756893280ULL, 2410249751631258ULL, 9426179177167454820ULL, 288302050835431937ULL, 1450724231204835328ULL, 4611723406130808834ULL, 1152979263872827456ULL, 576498755795288072ULL, 435423918817540ULL, 1173222921765667844ULL, 580559361577089ULL, 1153625364132798465ULL, 4620730622569616040ULL, 144134156866852904ULL, 2882303920456336448ULL, 5699883579871744ULL, 288336204817563904ULL, 1171252984647058470ULL, 76578854843875360ULL,
+};
+
 magic_entry rook_magics[64];
 magic_entry bishop_magics[64];
 
@@ -99,16 +106,16 @@ uint64_t project_bits(uint64_t mask, uint64_t bits){
 uint64_t rook_attack(uint64_t occ, int sq)
 {
     occ &= rook_magics[sq].mask;
-    return rook_table[sq][(occ * rook_magics[sq].magic) >> rook_magics[sq].shift];
+    return rook_table[sq][(occ * rook_magic_numbers[sq]) >> rook_magics[sq].shift];
 }
 
 uint64_t bishop_attack(uint64_t occ, int sq)
 {
     occ &= bishop_magics[sq].mask;
-    return bishop_table[sq][(occ * bishop_magics[sq].magic) >> bishop_magics[sq].shift];
+    return bishop_table[sq][(occ * bishop_magic_numbers[sq]) >> bishop_magics[sq].shift];
 }
 
-void fill_magics(){
+void find_magics(){
     uint64_t seed = 966479893026083835;
     for (int sq = 0; sq < 64; sq++){
         iteration_rook:
@@ -154,13 +161,53 @@ void fill_magics(){
         }
         bishop_magics[sq].magic = magic;
     }
+
+    printf("uint64_t rook_magic_numbers[64] = {\n    ");
+    for (int i = 0; i < 64; i++)
+    {
+        printf("%lluULL, ", rook_magics[i].magic);
+    }
+    printf("};\n\n");
+
+    printf("uint64_t bishop_magic_numbers[64] = {\n    ");
+    for (int i = 0; i < 64; i++)
+    {
+        printf("%lluULL, ", bishop_magics[i].magic);
+    }
+    printf("\n};\n\n");
+}
+
+void fill_tables()
+{
+    for (int sq = 0; sq < 64; sq++){
+        uint64_t ent = 1ULL << (64 - rook_magics[sq].shift);
+        memset(rook_table[sq], 0, sizeof(uint64_t) * 4096);
+        for (uint64_t i = 0; i < ent; i++){
+            uint64_t blocker_bb = project_bits(rook_magics[sq].mask, i);
+            uint64_t idx = (rook_magic_numbers[sq] * blocker_bb) >> rook_magics[sq].shift;
+            uint64_t* entry = &rook_table[sq][idx];
+            uint64_t attacks = rankAttacks(blocker_bb, sq) + fileAttacks(blocker_bb, sq);
+            *entry = attacks;
+        }
+    }
+    for (int sq = 0; sq < 64; sq++){
+        uint64_t ent = 1ULL << (64 - bishop_magics[sq].shift);
+        memset(bishop_table[sq], 0, sizeof(uint64_t) * 4096);
+        for (uint64_t i = 0; i < ent; i++){
+            uint64_t blocker_bb = project_bits(bishop_magics[sq].mask, i);
+            uint64_t idx = (bishop_magic_numbers[sq] * blocker_bb) >> bishop_magics[sq].shift;
+            uint64_t* entry = &bishop_table[sq][idx];
+            uint64_t attacks = diagonalAttacks(blocker_bb, sq) + antiDiagAttacks(blocker_bb, sq);
+            *entry = attacks;
+        }
+    }
 }
 
 void verify_magics()
 {
     for (int sq = 0; sq < 64; sq++)
     {
-        uint64_t magic = rook_magics[sq].magic;
+        uint64_t magic = rook_magic_numbers[sq];
         uint64_t ent = 1ULL << (64 - rook_magics[sq].shift);
         for (uint64_t i = 0; i < ent; i++)
         {
@@ -169,13 +216,14 @@ void verify_magics()
             uint64_t attacks = rankAttacks(blocker_bb, sq) + fileAttacks(blocker_bb, sq);
             if (rook_table[sq][idx] != attacks)
             {
-                printf("hmm");
+                printf("hmm\n");
             }
         }
     }
     for (int sq = 0; sq < 64; sq++)
     {
-        uint64_t magic = bishop_magics[sq].magic;
+
+        uint64_t magic = bishop_magic_numbers[sq];
         uint64_t ent = 1ULL << (64 - bishop_magics[sq].shift);
         for (uint64_t i = 0; i < ent; i++)
         {
@@ -333,6 +381,6 @@ static void init_table(void) {
         bishop_magics[sq].shift = 64 - __builtin_popcountll(bishop_attacks);
     }
 
-    fill_magics();
+    fill_tables();
     verify_magics();
 }
