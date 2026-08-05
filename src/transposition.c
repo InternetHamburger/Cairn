aab58edd657d7afc58558f08f3d16fc883a7044d
InternetHamburger <wolden.sindre@gmail.com>
2026-08-05 10:04:13 +0200

    Don't overwrite entries from pvs in qs bench 3983730

--- src/transposition.c
+++ src/transposition.c
@@ -17,3 +17,19 @@ bool IsNull(Entry entry)
 void ZeroTT(TT* tt){
     memset(tt->entries, 0, sizeof(Entry) * tt->num_entries);
 }
+
+void StoreTT(Entry prev_entry, uint64_t idx, int score, Move move, int depth, int type, Thread* thread)
+{
+    if (depth == 0 && GetDepth(prev_entry) > 0)
+    {
+        return;
+    }
+
+    const Entry entry = {
+        .hash = thread->board.zobrist_hash,
+        .best_move = move,
+        .score = (int16_t)score,
+        .depth_node_type = type | depth
+    };
+    thread->tt.entries[idx] = entry;
+}
--- src/transposition.h
+++ src/transposition.h
@@ -11,5 +11,6 @@ int GetDepth(Entry entry);
 int GetEntryType(Entry entry);
 void ZeroTT(TT* tt);
 bool IsNull(Entry entry);
+void StoreTT(Entry prev_entry, uint64_t idx, int score, Move move, int depth, int type, Thread* thread);
 
 #endif //CAIRN_TRANSPOSITION_H
